a08129fe120777dc1233ff403cdde8d00ae685c7
DomDemas <89409373+DomDemas@users.noreply.github.com>
2021-10-26 20:18:48 -0500

    problem 1 done

--- Problem2/MainWindow.xaml.cs
+++ Problem2/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Newtonsoft.Json;
 
 namespace Problem2
 {
@@ -22,34 +20,9 @@ namespace Problem2
     /// </summary>
     public partial class MainWindow : Window
     {
-        
-        
         public MainWindow()
         {
-            string url = "http://pcbstuou.w27.wh-2.com/webservices/3033/api/Wine/Reviews";
-            List<Wines> wine;
-
-            cboCountry.Items.Add("All");
-
-            using (var client = new HttpClient())
-            {
-                string json = client.GetStringAsync(url).Result;
-
-                wine = JsonConvert.DeserializeObject<List<Wines>>(json);
-                
-                foreach (var w in wine)
-                {
-                    lstWines.Items.Add(w);
-                    if (cboCountry.Items.Contains(w.country) == false)
-                    {
-                        cboCountry.Items.Add(w.country);
-                    }
-                    cboCountry.Items.Add(w.country);
-                }
-
-            }
+            InitializeComponent();
         }
-
-
     }
 }
